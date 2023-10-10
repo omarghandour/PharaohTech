a7d4459025050de37cdd68d5c4f1b4c77f73880e
omarghandour <109479370+omarghandour@users.noreply.github.com>
2023-10-11 02:17:35 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,30 +1,11 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import s from '../public/scarab-with-wings-svgrepo-com (1).svg'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Pharaoh Tech.',
   description: 'a company that creates websites for businesses of all sizes and sectors.',
-  icons: [
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '32x32',
-      url: s,
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '16x16',
-      url: s,
-    },
-    {
-      rel: 'apple-touch-icon',
-      sizes: '180x180',
-      url: s,
-    },
-  ],
+
 }
 
 export default function RootLayout({ children }) {
